b238f09dfa5106f1b6349eda7f52613859ef31f0
Drapeyko Nikita <drapeykonik@gmail.com>
2023-04-29 13:45:59 +0300

    Add classes for tranformations config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
 """File for experiment config defining"""
+from typing import Dict, List
+
 from pydantic import BaseModel
 
 
@@ -40,3 +42,41 @@ class DatasetConfig(BaseModel):
     train_batch: int = 8
     valid_batch: int = 1
     test_batch: int = 1
+
+
+class TransformConfig(BaseModel):
+    """
+    Class for defining transform config
+
+    Parameters
+    ----------
+    type: str
+        Type name of the transform
+    params: Dict
+        Params for the transformation. Exactly parameters
+        depends on the type of the transformation
+
+    """
+
+    transform: str
+    params: Dict
+
+
+class TransformationsConfig(BaseModel):
+    """
+    Class for defining tranformations config for
+    each dataset part
+
+    Parameters
+    ----------
+    train: List[TransformConfig]
+        Transform pipeline for the train dataset
+    valid: List[TransformConfig]
+        Transform pipeline for the valid dataset
+    test: List[TransformConfig]
+        Transform pipeline for the test dataset
+    """
+
+    train: List[TransformConfig]
+    valid: List[TransformConfig]
+    test: List[TransformConfig]
