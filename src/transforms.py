fa026fa85b937aac020b9790dd8b206d9be9b0f3
Drapeyko Nikita <drapeykonik@gmail.com>
2023-05-11 17:04:00 +0300

    Implement Resize transformation class

--- src/transforms.py
+++ src/transforms.py
@@ -2,7 +2,7 @@
 from typing import Any, Tuple
 
 import numpy as np
-from torchvision.transforms.functional import crop
+from torchvision.transforms.functional import crop, resize
 
 
 class Crop(object):
@@ -32,4 +32,32 @@ class Crop(object):
         image, location = sample
         image = crop(image, self.top, self.left, self.height, self.width)
         location = location - np.array([[self.left, self.top]])
+        return image, location
+
+
+class Resize(object):
+    """
+    Class implement transformation to resize sample from the dataset.
+    Provides image resizing and localization's mark coordinates modifying
+
+    Parameters
+    ----------
+    output_size: Union[int, tuple]
+        Output size of the image
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, tuple):
+            self.output_size = output_size
+        else:
+            self.output_size = (output_size, output_size)
+
+    def __call__(self, sample):
+        image, location = sample
+
+        w, h = image.size
+        new_h, new_w = self.output_size
+        image = resize(image, (new_h, new_w))
+        location = location * [new_w / w, new_h / h]
         return image, location
