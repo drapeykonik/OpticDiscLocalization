51dc68983f422296220592e5e14886a108f86545
Drapeyko Nikita <drapeykonik@gmail.com>
2023-05-13 12:51:34 +0300

    Implement valid_epoch function

--- src/vggregressor/pipeline.py
+++ src/vggregressor/pipeline.py
@@ -39,3 +39,35 @@ def train_epoch(
         loss.backward()
         optimizer.step()
     return train_losses_epoch
+
+
+def valid_epoch(
+    model: nn.Module,
+    criterion: Callable,
+    valid_loader: torch.utils.data.DataLoader,
+    device: str = "cuda",
+):
+    """
+    Function to perform one epoch of validation
+
+    Parameters
+    ----------
+    model: nn.Module
+        Model to validate
+    criterion: Callable
+        Loss function
+    valid_loader: torch.utils.data.DataLoader
+        Data loader object for validation set samples extraction
+    device: str, Optional
+        Device to perform validation. "cuda" default
+    """
+    valid_losses_epoch = []
+
+    model.eval()
+    with torch.no_grad():
+        for i, (images, locations) in enumerate(valid_loader):
+            pred_locations = model(images.to(device).float())
+            loss = criterion(locations.to(device).float(), pred_locations)
+            valid_losses_epoch.append(loss.item())
+
+    return valid_losses_epoch
