d657fb99ae41c410308453dfa22b5d7ccf08aa01
Drapeyko Nikita <drapeykonik@gmail.com>
2023-05-15 00:39:12 +0300

    Add experiments folder, test experiment and run module with args parser

---
+++ src/run.py
@@ -0,0 +1,10 @@
+import argparse
+import sys
+
+from exp_runner.runner import run
+
+parser = argparse.ArgumentParser(description="Runner")
+parser.add_argument("model")
+parser.add_argument("experiment")
+args = vars(parser.parse_args())
+run(args["model"], args["experiment"])
