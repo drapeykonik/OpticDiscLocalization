37deb8ac2c86e8aaf16ddcc778ff4c4e450b3c1b
Drapeyko Nikita <drapeykonik@gmail.com>
2023-05-22 23:25:26 +0300

    fixes to run ssd

--- src/exp_runner/runner.py
+++ src/exp_runner/runner.py
@@ -32,32 +32,4 @@ def run(model: str, experiment: str) -> None:
     config = Config.parse(config_file_path)
     exec("from " + model + ".pipeline import Pipeline")
     pipeline = eval("Pipeline(config)")
-    # pipeline.fit()
-    # pipeline.save_model(os.path.join(EXPERIMENTS_DIR, model, experiment))
-
-    # Just example
-    # image = Image.open("data/processed/localization/test/IDRiD_001.jpg")
-    # tfs_image, location = pipeline.evaluate(image)
-    # location = location.cpu().detach().numpy()
-    # fig, axes = plt.subplots(1, 2, figsize=(12, 16))
-    # axes[0].imshow(image)
-    # axes[0].set_title("Original")
-    # axes[1].imshow(tfs_image.permute(1, 2, 0).cpu().detach().numpy())
-    # axes[1].scatter(location[0][0], location[0][1], marker="+", s=100)
-    # axes[1].set_title("Transformed")
-    # plt.show()
-
-    image, location = pipeline.data_loaders["test"].dataset[0]
-    src_image, src_location = pipeline.inverse_transform(
-        "test", (image, location)
-    )
-    fig, axes = plt.subplots(1, 2, figsize=(12, 16))
-    axes[0].imshow(image.permute(1, 2, 0).numpy())
-    axes[0].scatter(
-        location.numpy()[0], location.numpy()[1], marker="+", s=100
-    )
-    axes[0].set_title("Source transformed")
-    axes[1].imshow(src_image)
-    axes[1].scatter(src_location[0], src_location[1], marker="+", s=100)
-    axes[1].set_title("Inverse transformed")
-    plt.show()
+    pipeline.fit()
--- src/ssd/pipeline.py
+++ src/ssd/pipeline.py
@@ -1,6 +1,10 @@
 import os
 
 import mmcv
+from mmdetection.mmdet.apis import (inference_detector, init_detector,
+                                    show_result_pyplot, train_detector)
+from mmdetection.mmdet.datasets import build_dataset
+from mmdetection.mmdet.models import build_detector
 
 from exp_runner.config import (Config, DataConfig, DatasetConfig,
                                LearningRateSchedulerConfig, LoggerConfig,
@@ -14,6 +18,20 @@ SSD_CONFIG = "mmdetection/configs/ssd/ssd512_coco.py"
 class Pipeline:
     def _init__(self, config: Config, log_dir: os.PathLike) -> None:
         self.config = mmcv.Config.fromfile(SSD_CONFIG)
+        self.config.work_dir = log_dir
+        self.load_from = "ssd/mmdetection/checkpoints/ssd512.pth"
+
+        self.datasets = dict()
+        for part in ("train", "valid", "test"):
+            self.datasets[part] = build_dataset(self.config.data[part])
+
+        self.model = build_detector(
+            self.config.model,
+            train_cfg=self.config.get("train_cfg"),
+            valid_cfg=self.config.get("valid_cfg"),
+            test_cfg=self.config.get("test_cfg"),
+        )
+        self.model.CLASSES = self.datasets["train"].METAINFO["classes"]
 
     def __configure_pipeline(self, pipeline_config: PipelineConfig) -> None:
         self.config.device = pipeline_config.device
@@ -53,3 +71,17 @@ class Pipeline:
         self.config.optimizer = dict(
             type=optimizer_config.type, **optimizer_config.params
         )
+
+    def fit(self):
+        train_detector(
+            self.model,
+            self.datasets.values(),
+            self.config,
+            distributed=False,
+            validate=True,
+        )
+
+    def evaluate(self, image_path: os.PathLike):
+        image = mmcv.imread(image_path)
+        result = inference_detector(self.model, image)
+        show_result_pyplot(self.model, image, result, score_thr=0.8)
